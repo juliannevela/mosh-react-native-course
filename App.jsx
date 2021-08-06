1b6adbeda73a306126f75230e7a277366bb9b4cd
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-08-05 19:48:57 -0700

    finished layout lessons - working on exercises

--- App.jsx
+++ App.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
-import {
-    Text,
-    Image,
-    SafeAreaView,
-    TouchableOpacity,
-    TouchableNativeFeedback,
-    View,
-} from 'react-native';
-import styles from './styles/app-style';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Hello React Native</Text>
-            <TouchableNativeFeedback
-                onPress={() => console.log('image pressed')}
-            >
-                <View
-                    style={{
-                        backgroundColor: 'yellow',
-                        height: 200,
-                        width: 200,
-                    }}
-                />
-            </TouchableNativeFeedback>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <View style={styles.one} />
+            <View style={styles.two} />
+            <View style={styles.three} />
+            <View style={styles.four} />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    one: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+        alignSelf: 'flex-start',
+    },
+    two: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'yellow',
+    },
+    three: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'green',
+    },
+    four: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+    },
+});
