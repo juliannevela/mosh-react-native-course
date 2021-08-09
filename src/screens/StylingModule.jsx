08489b116e9319d51feb00da5a50dae83225cae3
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-08-09 16:45:18 -0700

    encapsulating styles

--- src/screens/StylingModule.jsx
+++ src/screens/StylingModule.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppText from '../components/AppText';
+
 export default function StylingModule() {
     return (
         <View style={styles.container}>
-            <Text style={styles.inner}>
-                I love React Native! This is my first React Native app. This is
-                going to be really awesome when it's finished being built!'
-            </Text>
+            <AppText style={styles.inner}>I love React Native!</AppText>
         </View>
     );
 }
@@ -21,10 +20,5 @@ const styles = StyleSheet.create({
     inner: {
         fontSize: 20,
         fontFamily: 'Roboto',
-        fontStyle: 'italic',
-        fontWeight: 'bold',
-        color: '#FF00FF',
-        textTransform: 'capitalize',
-        textAlign: 'center',
     },
 });
