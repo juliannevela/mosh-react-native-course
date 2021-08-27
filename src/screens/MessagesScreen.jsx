102ce50f4b9bec3dea5d56f41d6e875dd4af2087
Julianne Vela <75726146+juliannevela@users.noreply.github.com>
2021-08-26 22:50:30 -0700

    working delete swipable action

--- src/screens/MessagesScreen.jsx
+++ src/screens/MessagesScreen.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
 
 import ListItem from '../components/ListItem';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
-const messages = [
+const InitialMessages = [
     {
         id: 1,
         title: 'Message 1',
@@ -22,6 +22,14 @@ const messages = [
 ];
 
 export default function MessagesScreen() {
+    const [messages, setMessages] = useState(InitialMessages);
+
+    const handleDelete = (message) => {
+        // Delete message from messages
+        // and update the list
+        setMessages(messages.filter((m) => m.id !== message.id));
+    };
+
     return (
         <Screen>
             <FlatList
@@ -33,10 +41,14 @@ export default function MessagesScreen() {
                         description={item.description}
                         image={item.image}
                         onPress={() => console.log('Pressed: ', item)}
-                        renderRightActions={ListItemDeleteAction}
+                        renderRightActions={() => (
+                            <ListItemDeleteAction
+                                onPress={() => handleDelete(item)}
+                            />
+                        )}
+                        ItemSeparatorComponent={ListItemSeparator}
                     />
                 )}
-                ItemSeparatorComponent={ListItemSeparator}
             />
         </Screen>
     );
